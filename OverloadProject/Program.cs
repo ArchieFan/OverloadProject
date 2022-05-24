5f281c187e308df1d233632b26b6953288f02914
Archie Fan <archiefan@gmail.com>
2022-05-24 10:27:13 +0100

    Add project files.

---
+++ OverloadProject/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadProject
+{
+    internal class Program
+    {
+        public void Test()
+        {
+            Console.WriteLine("1st Method");
+        }
+
+        public void Test(int i)
+        {
+            Console.WriteLine($"2nd Method {i}");
+        }
+
+        public void Test(string s)
+        {
+            Console.WriteLine($"3rd Method {s}");
+        }
+
+        public void Test(int i, string s)
+        {
+            Console.WriteLine($"4th Method {s}");
+        }
+
+        public void Test(string s, int i)
+        {
+            Console.WriteLine($"5th Method {s}");
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Test();
+            Console.ReadLine();
+        }
+    }
+}
